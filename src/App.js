5a7bede6d216672ac64cb717615d9899813f3178
jbharwood <josephblakeharwood@gmail.com>
2019-05-24 12:06:11 -0400

    comment cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Navbar from './components/Navbar'
 import Dashboard from './components/Dashboard'
-// import Search from './components/Search'
-// import MovieView from './components/MovieView'
-// import Watchlist from './components/Watchlist'
-// import LoginForm from './components/LoginForm'
-// import SignUpForm from './components/SignUpForm'
-// import Feed from './components/Feed'
 import 'semantic-ui-css/semantic.min.css';
 import {connect} from 'react-redux'
 import { Switch, Route } from 'react-router-dom'
@@ -25,12 +18,9 @@ class App extends Component {
 		this.setState({
 			currentUser: response
 		})
-		// this.props.dispatch({type: "ADD_TO_USERS", payload: response})
 	}
 
   logout = () => {
-		// let index = this.props.users.indexOf(this.props.user);
-		// this.props.dispatch({type: "REMOVE_USER_FROM_USERS", payload: index})
 		localStorage.clear()
 		this.deleteFromFeedUser()
     this.props.dispatch({type: "SET_CURRENT_USER", payload: null})
@@ -40,7 +30,6 @@ class App extends Component {
 	}
 
 	deleteFromFeedUser = () => {
-		// fetch(`http://localhost:3000/feed_users/${1}`, {
 		fetch(`http://localhost:3000/feed_users/${this.props.feedUser.id}`, {
       method: "DELETE"
     })
@@ -66,46 +55,7 @@ class App extends Component {
 		}
 	}
 
-
-		// var test2 = "#0066ff"
-
-	// var theme2 =
-	// 	{
-	// 		palette: {
-	// 			primary: {
-	// 				// light: will be calculated from palette.primary.main,
-	// 				main: test2,
-	// 				// dark: will be calculated from palette.primary.main,
-	// 				// contrastText: will be calculated to contrast with palette.primary.main
-	// 			},
-	// 			secondary: {
-	// 				light: '#0066ff',
-	// 				main: '#0044ff',
-	// 				// dark: will be calculated from palette.secondary.main,
-	// 				contrastText: '#ffcc00',
-	// 			},
-	// 			// error: will use the default color
-	// 		},
-	// 	}
-
-		// theme = () => {
-		// 	createMuiTheme(theme1);
-		// }
-		// const themea = createMuiTheme(theme2);
-
-
-	// renderPic = () => {
-	// 	let container = document.querySelector("div.Dashboard-tableContainer-14")
-	// 	// debugger
-	// 	if (container !== null && this.props.isHidden === false) {
-	// 		container.innerHTML = "<img src=https://visualhunt.com/photos/1/night-television-tv-video.jpg?s=l className=mainImage width=100% height=100%>"
-	// 	} else if (container !== null && this.props.isHidden === true) {
-	// 		container.innerHTML = ""
-	// 	}
-	// }
-
 	componentDidMount() {
-		// //this.props.dispatch({type: "HIDE_APP", payload: false})
 		const jwt = localStorage.getItem('jwt')
 		if (jwt){
 			fetch("http://localhost:3000/auto_login", {
@@ -125,17 +75,6 @@ class App extends Component {
 		}
 	}
 
-	// changeThing = () => {
-	// 	test = "#A22424"
-	// 	test2 = "#EBDDDD"
-	// 	console.log(test);
-	// 	console.log(test2);
-	// }
-
-	// theme = () => {
-	// 	createMuiTheme(theme1);
-	// }
-
   render() {
     return (
       <div className="App">
@@ -149,78 +88,7 @@ class App extends Component {
   }
 }
 
-var test = "#323232"
-// var test = "#323232"
-
-// "#323232"
-// var theme1 =
-// 	{
-// 		palette: {
-// 			primary: {
-// 				// light: will be calculated from palette.primary.main,
-// 				main: test,
-// 				// dark: will be calculated from palette.primary.main,
-// 				// contrastText: will be calculated to contrast with palette.primary.main
-// 			},
-// 			secondary: {
-// 				light: '#0066ff',
-// 				main: '#0044ff',
-// 				// dark: will be calculated from palette.secondary.main,
-// 				contrastText: '#ffcc00',
-// 			},
-// 			// error: will use the default color
-// 		},
-// 	}
-//
-// 	var test2 = "#0066ff"
-//
-// var theme2 =
-// 	{
-// 		palette: {
-// 			primary: {
-// 				// light: will be calculated from palette.primary.main,
-// 				main: test2,
-// 				// dark: will be calculated from palette.primary.main,
-// 				// contrastText: will be calculated to contrast with palette.primary.main
-// 			},
-// 			secondary: {
-// 				light: '#0066ff',
-// 				main: '#0044ff',
-// 				// dark: will be calculated from palette.secondary.main,
-// 				contrastText: '#ffcc00',
-// 			},
-// 			// error: will use the default color
-// 		},
-// 	}
-//
-// 	const theme = createMuiTheme(theme1);
-// 	const themea = createMuiTheme(theme2);
-
-//   render() {
-//     return (
-//       <div className="App">
-//       <h1>Cinematch</h1>
-//   			<Grid>
-//   				<Dashboard currentUser={this.state.currentuser} logout={this.logout} changePage={this.changePage}/>
-//   				<Grid.Row centered>
-//   					<Switch>
-//               <Route path="/watchlist" render={routerProps => <Watchlist changePage={this.changePage} {...routerProps} />} />
-//               <Route path="/login" render={routerProps => <LoginForm {...routerProps} setCurrentUser={this.setCurrentUser}
-//                 changePage={this.changePage} />} />
-//               <Route path="/signup" render={routerProps => <SignUpForm {...routerProps} setCurrentUser={this.setCurrentUser}
-//                 changePage={this.changePage} />} />
-//   						<Route path="/search" render={routerProps => <Search changePage={this.changePage} {...routerProps} />} />
-//   						<Route path="/chatbox" render={routerProps => <Feed changePage={this.changePage} {...routerProps} />} />
-//   					</Switch>
-//   				</Grid.Row>
-//   			</Grid>
-//       </div>
-//
-//     );
-//   }
-// }
-
-
+// var defaultColor = "#323232"
 
 function mapStateToProps(state){
   return {
@@ -232,7 +100,5 @@ function mapStateToProps(state){
 		primaryColor: state.primaryColor
   }
 }
-
-// "#323232"
 
 export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -9,30 +9,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { ActionCableProvider } from 'react-actioncable-provider'
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux'
-// import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-// import {connect} from 'react-redux'
-
-
-// let primaryColor = "#323232"
-
-
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: {
-//       // light: will be calculated from palette.primary.main,
-//       main: primaryColor,
-//       // dark: will be calculated from palette.primary.main,
-//       // contrastText: will be calculated to contrast with palette.primary.main
-//     },
-//     secondary: {
-//       light: '#0066ff',
-//       main: '#0044ff',
-//       // dark: will be calculated from palette.secondary.main,
-//       contrastText: '#ffcc00',
-//     },
-//     // error: will use the default color
-//   },
-// });
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
@@ -44,17 +20,3 @@ ReactDOM.render(
       </Provider>
     </ActionCableProvider>
   </Router>, document.getElementById('root'));
-
-// ReactDOM.render(
-//   <Router>
-//     <ActionCableProvider url={'ws://localhost:3000/cable'}>
-//       <Provider store={store}>
-//         <MuiThemeProvider theme={theme}>
-//           <Route path="/" render={routerProps => <App primaryColor={primaryColor} {...routerProps} />} /> />
-//         </MuiThemeProvider>,
-//       </Provider>
-//     </ActionCableProvider>
-//   </Router>, document.getElementById('root'));
-
-// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-// serviceWorker.unregister()
