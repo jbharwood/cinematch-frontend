d8608b5b1d1ee7ce6de5e244f0ff4944ad88a392
jbharwood <josephblakeharwood@gmail.com>
2019-06-05 17:25:24 -0400

    changed index websocket back to wss

--- src/index.js
+++ src/index.js
@@ -13,13 +13,11 @@ import { createStore, applyMiddleware } from 'redux'
 const store = createStore(reducer, applyMiddleware(thunk));
 
 // <ActionCableProvider url={'ws://localhost:3000/cable'}>
-// <ActionCableProvider url={"wss://cinematch-jbharwood.herokuapp.com/"}>
-
 
 
 ReactDOM.render(
   <Router>
-    <ActionCableProvider url={"ws://cinematch-jbharwood.herokuapp.com/"}>
+    <ActionCableProvider url={"wss://cinematch-jbharwood.herokuapp.com/"}>
       <Provider store={store}>
           <Route path="/" render={routerProps => <App {...routerProps} />} /> />
       </Provider>
