10c3854427fa29d640e2328f438ea8766545bd24
jbharwood <josephblakeharwood@gmail.com>
2019-06-05 17:20:44 -0400

    changed index.js actioncable ws

--- src/index.js
+++ src/index.js
@@ -13,11 +13,13 @@ import { createStore, applyMiddleware } from 'redux'
 const store = createStore(reducer, applyMiddleware(thunk));
 
 // <ActionCableProvider url={'ws://localhost:3000/cable'}>
+// <ActionCableProvider url={"wss://cinematch-jbharwood.herokuapp.com/"}>
+
 
 
 ReactDOM.render(
   <Router>
-    <ActionCableProvider url={"wss://cinematch-jbharwood.herokuapp.com/"}>
+    <ActionCableProvider url={"ws://cinematch-jbharwood.herokuapp.com/"}>
       <Provider store={store}>
           <Route path="/" render={routerProps => <App {...routerProps} />} /> />
       </Provider>
