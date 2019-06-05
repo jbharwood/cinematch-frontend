ed7838dc45fa3f1c9349ceb2441594586366d6f1
jbharwood <josephblakeharwood@gmail.com>
2019-06-05 16:48:03 -0400

    removed src from gitignore to add config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+let API_URL
+if (process.env["NODE_ENV"] === "development") {
+  API_URL = "http://localhost:3000"
+} else if (process.env["NODE_ENV"] === "production") {
+  API_URL = "https://cinematch-api.herokuapp.com"
+}
+
+export default API_URL
