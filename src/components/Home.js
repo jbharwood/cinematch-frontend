1826686683fb0eda47606efa5fa56de354216da2
jbharwood <josephblakeharwood@gmail.com>
2019-07-26 17:13:20 -0400

    removed NewPostInfo component from project

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,6 @@ import {connect} from 'react-redux'
 const Home = (props) => {
 
   const renderPic = () => {
-		// let container = document.querySelector("div.Dashboard-tableContainer-14")
-    // if (container !== null) {
-    //   container.innerHTML = "<img src=https://visualhunt.com/photos/1/night-television-tv-video.jpg?s=l className=mainImage width=100% height=100%>"
-    // }
     return "<img src=https://visualhunt.com/photos/1/night-television-tv-video.jpg?s=l className=mainImage width=100% height=100%>"
 	}
 
